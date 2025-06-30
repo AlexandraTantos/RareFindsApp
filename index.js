65a38608373dc12e23ed56a05002ade46671b65c
Tanțoș Alexandra-Bianca <114863064+AlexandraTantos@users.noreply.github.com>
2025-06-30 21:13:16 +0300

    Add products page UI

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
-const toggleNavBtn = document.querySelector(".toggle-nav");
-const navLinks = document.querySelector(".nav-links");
+import { products } from "./data.js";
 
-toggleNavBtn.addEventListener("click", () => {
-  navLinks.classList.toggle("show");
+window.addEventListener("DOMContentLoaded", function () {
+  const toggleButton = document.querySelector(".toggle-nav");
+  const navLinks = document.querySelector(".nav-links");
+  const productsPageSection = document.querySelector("#products-page");
+  const featuredSection = document.querySelector("#featured");
+
+  toggleButton.addEventListener("click", () => {
+    navLinks.classList.toggle("hidden");
+  });
+
+  if (featuredSection) {
+    displayProducts(products.slice(0, 4), featuredSection);
+  }
+
+  if (productsPageSection && products.length > 0) {
+    displayProducts(products, productsPageSection);
+  }
 });
+
+const displayProducts = (products, container) => {
+  const displayProduct = products
+    .map((item) => {
+      return `
+        <div class="product-card">
+  <img src="${item.image}" alt="${item.name}" />
+  <h4>${item.name}</h4>
+  <p class="brand">Brand: ${item.brand}</p>
+  <p class="price">Price: ${item.price}</p>
+  <p class="rating">Rating: ${item.rating}</p>
+</div>
+`;
+    })
+    .join("");
+
+  container.innerHTML = `<div class="product-list">${displayProduct}</div>`;
+};
