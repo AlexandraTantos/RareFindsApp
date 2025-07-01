9d44f47b2eb23784232db5a87a8d102b37a37115
Tanțoș Alexandra-Bianca <114863064+AlexandraTantos@users.noreply.github.com>
2025-07-01 17:26:57 +0300

    Update product card UI and implement search bar functionality

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ window.addEventListener("DOMContentLoaded", function () {
   const navLinks = document.querySelector(".nav-links");
   const productsPageSection = document.querySelector("#products-page");
   const featuredSection = document.querySelector("#featured");
+  const searchInput = document.querySelector(".input");
 
   toggleButton.addEventListener("click", () => {
     navLinks.classList.toggle("hidden");
@@ -17,18 +18,60 @@ window.addEventListener("DOMContentLoaded", function () {
   if (productsPageSection && products.length > 0) {
     displayProducts(products, productsPageSection);
   }
+
+  searchInput.addEventListener("input", (e) => {
+    let value = e.target.value.toLowerCase().trim();
+
+    const filtered = products.filter(
+      (product) =>
+        product.name.toLowerCase().includes(value) ||
+        product.brand.toLowerCase().includes(value)
+    );
+
+    if (productsPageSection) {
+      if (filtered.length > 0) {
+        displayProducts(filtered, productsPageSection);
+      } else {
+        productsPageSection.innerHTML = `<p class="no-results">No products found!</p>`;
+      }
+    }
+  });
 });
 
 const displayProducts = (products, container) => {
   const displayProduct = products
     .map((item) => {
       return `
-        <div class="product-card">
-  <img src="${item.image}" alt="${item.name}" />
-  <h4>${item.name}</h4>
-  <p class="brand">Brand: ${item.brand}</p>
-  <p class="price">Price: ${item.price}</p>
-  <p class="rating">Rating: ${item.rating}</p>
+       <div class="card">
+ <div class="tilt">
+  <div class="img"><img src="${item.image}" alt="${item.image}" ></div>
+ </div>
+ <div class="info">
+  <div class="cat">${item.brand}</div>
+  <h2 class="title">${item.name}</h2>
+  <p class="desc">${item.description}</p>
+  
+  <div class="bottom">
+   <div class="price">
+    <span class="old">$${item.oldPrice}</span>
+    <span class="new">$${item.price}</span>
+   </div>
+   <button class="btn">
+    <span>Add to Cart</span>
+    <svg class="icon" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+     <path d="M6 2L3 6v14a2 2 0 002 2h14a2 2 0 002-2V6l-3-4"/>
+     <line x1="3" y1="6" x2="21" y2="6"/>
+     <path d="M16 10a4 4 0 01-8 0"/>
+    </svg>
+   </button>
+  </div>
+  <div class="meta">
+   <div class="rating">
+      <p> Rating ${item.rating}</p>
+   </div>
+   <div class="stock">In Stock</div>
+  </div>
+ </div>
 </div>
 `;
     })
